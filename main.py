cca673f19e384db17fc839f77e44aacc22d6e34e
koby <kobyisr1@gmail.com>
2025-12-02 13:12:03 +0200

    start and configur unicorn

--- main.py
+++ main.py
@@ -1,39 +1,4 @@
-from fastapi import FastAPI
-from database import engine, SessionLocal
-import models
-from routers import auth, admin, users
-from contextlib import asynccontextmanager
-from fastapi.middleware.cors import CORSMiddleware
-from app.core.config import settings
+import uvicorn
 
-# Ensure database tables are created
-models.Base.metadata.create_all(bind=engine)
-
-# Lifespan Event (Manages DB Connections)
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    print("🚀 FastAPI application starting...")
-    yield
-    print("🛑 FastAPI application shutting down...")
-    # Ensure DB session cleanup
-    SessionLocal().close()
-
-# Create FastAPI App
-app = FastAPI(lifespan=lifespan)
-
-# Define Root Route
-@app.get("/")
-def root():
-    return {"message": "Welcome to our Blockchain application!"}
-
-# Register API Routers
-app.include_router(auth.router)
-app.include_router(admin.router)
-app.include_router(users.router)
-
-app.add_middleware(CORSMiddleware,
-    allow_origins=settings.CORS_ORIGINS,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+if __name__ == "__main__":
+    uvicorn.run("app.app:app", host ="0.0.0.0", port=8000, reload=True)
