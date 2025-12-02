34af975bc3c8f1a413bd9f1046a5b71edc27d7fe
koby <kobyisr1@gmail.com>
2025-12-02 13:11:14 +0200

    get url from setting class

--- app/database/db_config.py
+++ app/database/db_config.py
@@ -1,8 +1,9 @@
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from app.configuration.config import settings
 
-SQLALCHEMY_DATABASE_URL="sqlite:///./smartloans.db"
+SQLALCHEMY_DATABASE_URL=settings.DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':False})
 
