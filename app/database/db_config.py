cc257724bc0474fee8aa6e5213c039f068aa0d8f
koby <kobyisr1@gmail.com>
2025-12-03 14:48:28 +0200

    upgrade start engine to work with any db

--- app/database/db_config.py
+++ app/database/db_config.py
@@ -5,7 +5,13 @@ from app.configuration.config import settings
 
 SQLALCHEMY_DATABASE_URL=settings.DATABASE_URL
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':False})
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL,
+        connect_args={"check_same_thread": False},
+    )
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit = False, autoflush=False, bind=engine)
 
